d22dcc7a96e869b62a5fe8645d2cf8df853e9cc4
EgorGer <QwertiQQQ@gmal.com>
2024-10-09 09:46:29 +0300

    feat:add functiom reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    const numberToDelete = "-";
+    const reversString = String(n).split("")
+    const filteredNumbers = reversString.filter((number) => number !== numberToDelete);
+    return filteredNumbers.reverse().join('')
 }
